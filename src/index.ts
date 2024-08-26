e880469d602725e79631cc0eb96995ba5c1a4fe4
inuoshios <inuoshios@gmail.com>
2024-08-27 00:09:36 +0100

    chore: adding request body to the log message

--- src/index.ts
+++ src/index.ts
@@ -10,15 +10,23 @@ export interface BritanniaOptions {
 export default function britannia(options?: BritanniaOptions) {
   return (req: IncomingMessage, res: ServerResponse, next: () => void) => {
     const start = process.hrtime.bigint();
+    let body = '';
+    req.on('data', (chunk) => {
+      body += chunk.toString();
+    });
+
 
     res.on('finish', () => {
       const end = process.hrtime.bigint();
       const duration = Number(end - start) / 1_000_000;
       const url = `${(req.headers['x-forwarded-proto'] as string) === "https" ? "https" : "http"}://${req.headers.host}/${req.url}`;
-      const logMessage = `${req.method}\t${res.statusCode}\t${url}\t${duration.toFixed(2)}ms\t${new Date().toLocaleDateString()}\n`;
+      let logMessage = `${req.method}\t${res.statusCode}\t${url}\t${duration.toFixed(2)}ms\t${new Date().toLocaleDateString()}`;
       if (options?.showRequestBody) {
-        const logMessage = `${req.method}\t${res.statusCode}\t${url}\t${duration.toFixed(2)}ms\t${new Date().toLocaleDateString()}\t${}\n`;
+        logMessage += `\t${body}`;
       }
+      logMessage += `\n`;
+
+      // write to the file
       if (options && options.writeToFile) {
         fs.appendFile('britannia.log' ?? options.fileName, logMessage, { flag: 'a+' }, (err) => {
           if (err) {
