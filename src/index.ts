ebc499f954fee7981e0ab084e33ef0c3fe37d68e
inuoshios <inuoshios@gmail.com>
2024-08-26 23:59:24 +0100

    chore: adding request body func

--- src/index.ts
+++ src/index.ts
@@ -14,18 +14,21 @@ export default function britannia(options?: BritanniaOptions) {
     res.on('finish', () => {
       const end = process.hrtime.bigint();
       const duration = Number(end - start) / 1_000_000;
-      const url = `${(req.headers['x-forwarded-proto'] as string) === "https" ? "https" : "http"}://${req.headers.host}/${req.url}`
-      const logMessage = `${req.method}\t${res.statusCode}\t${url}\t${duration.toFixed(2)}ms\t${new Date().toLocaleDateString()}\n`
+      const url = `${(req.headers['x-forwarded-proto'] as string) === "https" ? "https" : "http"}://${req.headers.host}/${req.url}`;
+      const logMessage = `${req.method}\t${res.statusCode}\t${url}\t${duration.toFixed(2)}ms\t${new Date().toLocaleDateString()}\n`;
+      if (options?.showRequestBody) {
+        const logMessage = `${req.method}\t${res.statusCode}\t${url}\t${duration.toFixed(2)}ms\t${new Date().toLocaleDateString()}\t${}\n`;
+      }
       if (options && options.writeToFile) {
         fs.appendFile('britannia.log' ?? options.fileName, logMessage, { flag: 'a+' }, (err) => {
           if (err) {
             process.stderr.write(`Error while writing to file ${JSON.stringify({ reason: err.message })}\n`);
           }
-        })
+        });
       }
       process.stdout.write(logMessage);
     });
 
     next();
-  }
+  };
 }
